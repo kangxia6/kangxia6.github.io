d24d9bb9ba8b4a6e4eac0c59a4e93447b1204c85
kangxia6 <74895134+kangxia6@users.noreply.github.com>
2021-10-03 16:26:15 +0800

    康夏
    10.3

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var logo = document.querySelector('.logo');
 var dingbu = document.querySelector('.dingbu');
 var zhu1 = document.querySelector('.zhu1');
+var zhu2 = document.querySelector('.zhu2');
 var cs = document.querySelector('.cs');
 var gy = document.querySelector('.guanyu');
 var tc = document.querySelector('.tanchuang');
@@ -9,7 +10,37 @@ var bottom = document.querySelector('.bottom');
 var box = document.querySelector('.box');
 var time = document.querySelector('.time');
 var a = 1;
+//返回顶部
+window.addEventListener("scroll",handle)
+            function handle(){
+                //这样写是为了兼容性
+                //要获取当前的滚动条纵坐标位置，documentElement对应的是html标签
+                //body对应的是body标签
+                //IE及更早IE版本不支持,但可以使用document.documentElement.scrollTop
+                var oTop = document.body.scrollTop||document.documentElement.scrollTop
+                if(oTop>=200){
+                    gotop.style.display = "block";
+                    
+                    dingbu.style.animation = 'dingbu1 1s linear forwards';
+					zhu1.style.animation = 'zhu11 1s linear forwards';
+					bottom.style.animation = 'zhu11 1s linear forwards';
+					logo.style.animation = 'logo 1s linear forwards';    
+                }else{
+                    gotop.style.display = "none";
+                    
+                    dingbu.style.animation = 'dingbu 1s linear forwards';
+					zhu1.style.animation = 'zhu1 1s linear forwards';
+					bottom.style.animation = 'zhu1 1s linear forwards';
+					logo.style.animation = 'logo2 1s linear forwards';
+                }
 
+                gotop.onclick = function(){
+                    window.scrollTo({
+                        top:0,
+                        behavior:"smooth"
+                    })
+                }
+            }
 //logo的点击事件
 logo.onclick = function() {
 
@@ -21,14 +52,14 @@ logo.onclick = function() {
 		zhu1.style.animation = 'zhu1 1s linear forwards';
 		bottom.style.animation = 'zhu1 1s linear forwards';
 		logo.style.animation = 'logo2 1s linear forwards';
-
+//		alert('111');
 		a = 1;
 	} else {
 		dingbu.style.animation = 'dingbu1 1s linear forwards';
 		zhu1.style.animation = 'zhu11 1s linear forwards';
 		bottom.style.animation = 'zhu11 1s linear forwards';
 		logo.style.animation = 'logo 1s linear forwards';
-
+//		alert('222');
 		//		logo.style.left='50px';
 		a = 0;
 	}
@@ -82,7 +113,7 @@ end1.onclick = function() {
 	tc1.style.display = 'none';
 }
 //end
-排行榜
+//排行榜
 function changebox(n) {
 	var i = 1;
 	while(true) {
--- js/qiehuan.js
+++ js/qiehuan.js
@@ -4,6 +4,13 @@ let colors = ["#AEF7FF", "#717171", "#FFE8BF", "#f7cfc0", "#373d54", "#efe8de","
   buttons = Array.from(document.querySelectorAll("button")),
   body = document.body;
 
+
+var colorr = ''
+
+function tz(a){
+	window.location.href = a+'?'+colorr;
+}
+
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
     btn.classList.toggle("active");
@@ -17,10 +24,14 @@ buttons.forEach((btn) => {
       highlightColor = "#2DE6FE";
     }
     document.documentElement.style.setProperty("--bg", color);
+    colorr = color;
     document.documentElement.style.setProperty("--base", textColor);
+//  textColorr = textColor;
     document.documentElement.style.setProperty("--highlight", highlightColor);
   });
+  
 });
+
 
 function getCorrectTextColor(hex) {
   threshold = 130; /* about half of 256. Lower threshold equals more dark text on dark background  */
@@ -48,4 +59,189 @@ function getCorrectTextColor(hex) {
   } else {
     return "#fff";
   }
+}
+
+
+window.onload = function(){
+	var tt = window.location.href
+	console.log(tt)
+	var colorrr = '';
+	colorA = tt.split('#')
+	if(!colorA[1] || colorA[1] == 'undefined'){
+		document.documentElement.style.setProperty("--bg",'#ebecf0');
+//		console.log('哈哈哈')
+	}else{
+	document.documentElement.style.setProperty("--bg",'#' + colorA[1]);
+	};
+	if(colorA[1]=='717171'||colorA[1]=='373d54'){
+		document.documentElement.style.setProperty("--base", '#fff');
+	}
+	colorr = '#' + colorA[1];
+
+//	console.log('变没变')
+	/*  轮播图*/
+	
+	
+        let timer = setInterval(get_next, 3000)
+        let sz = new Array();
+        let szdiv = new Array()
+        var cur_ul = document.getElementById("banner");
+        
+    
+
+        
+        for (let i = 1; i <= 5; i++) {
+            var cur_li = document.createElement("li");
+            var cur_img = document.createElement("img");
+      
+            cur_img.src = "img/banner/" + i + ".jpg";
+            cur_img.style.width = "500px";
+            cur_img.style.height = "230px";
+            cur_img.style.borderRadius = "10px";
+            cur_img.style.margin = "-20px";
+            cur_img.style.marginLeft = "-50px";
+            cur_img.style.background = "#fff";
+            cur_img.style.boxShadow = "-2px -2px 6px rgb(255, 255, 255),4px 4px 12px rgba(121, 130, 160, 0.747)";
+            cur_img.style.border = "5px solid var(--bg)";
+            cur_li.appendChild(cur_img);
+            cur_li.onmouseenter = function () {
+                clearInterval(timer);
+            }
+            cur_li.onmouseleave = function () {
+                timer = setInterval(get_next, 3000)
+            }
+
+            if (i != 5) {
+                cur_li.id = 5 - i;
+            } else {
+                cur_li.id = 5;
+            }
+
+            cur_ul.appendChild(cur_li)
+            sz.push(cur_li);
+            sz[sz.length - 1].style.left = "0px";
+            let bottom_div = document.createElement("div");
+            bottom_div.style.left = 20 * i + "px";
+            bottom_div.style.marginLeft = "340px";
+            bottom_div.style.border = "3.5px solid var(--bg)";
+            bottom_div.name = i;
+            szdiv.push(bottom_div)
+            cur_ul.appendChild(bottom_div);
+
+        }
+
+        let pre_img = document.createElement("img")
+        pre_img.src = "img/preImg.png";
+        pre_img.style.position = "absolute";
+        pre_img.style.left = "-50px";
+        pre_img.style.width = "20px";
+        pre_img.style.top = 0;
+        pre_img.style.top = 0;
+        pre_img.style.bottom = 0;
+        pre_img.style.margin = "auto"
+        pre_img.style.zIndex = 10;
+        cur_ul.appendChild(pre_img);
+
+        let nex_img = document.createElement("img")
+        nex_img.src = "img/nexImg.png";
+        nex_img.style.position = "absolute";
+        nex_img.style.right = "-50px";
+        nex_img.style.width = "20px";
+        nex_img.style.top = 0;
+        nex_img.style.bottom = 0;
+        nex_img.style.margin = "auto"
+        nex_img.style.zIndex = 10;
+        cur_ul.appendChild(nex_img);
+
+        pre_img.onclick = function () {
+            clearInterval(timer);
+            get_pre();
+            timer = setInterval(get_next, 3000)
+        }
+
+        nex_img.onclick = function () {
+            clearInterval(timer);
+            get_next();
+            timer = setInterval(get_next, 3000)
+        }
+
+
+        let len = sz.length - 1;
+        sz[len - 2].style.left = "-80px";
+        sz[len - 1].style.zIndex = 10;
+        sz[len - 1].style.left = "200px";
+        sz[len - 1].style.transform = "scale(1.3)";
+        sz[len].style.left = "480px";
+
+        szdiv[0].style.background = "#e431fc"
+
+        for (let i = 0; i < szdiv.length; i++) {
+            szdiv[i].onmouseenter = function () {
+                clearInterval(timer);
+                let len1 = sz[len - 1].id;
+                let len2 = szdiv[i].name;
+                let dis = Math.max(len1, len2) - Math.min(len1, len2)
+                if (len1 > len2) {
+                    while (dis--)
+                        get_pre()
+                } else {
+                    while (dis--)
+                        get_next()
+                }
+                timer = setInterval(get_next,3000)
+            }
+        }
+
+
+        function get_pre() {
+            let give_up = sz[0];
+            sz.shift()
+            sz.push(give_up)
+            for (let i = 0; i < sz.length; i++) {
+                sz[i].style.zIndex = i;
+                sz[i].style.transform = "scale(1)"
+
+            }
+            sz[len - 2].style.left = "-80px";
+            sz[len - 1].style.zIndex = 10
+            sz[len - 1].style.left = "200px";
+            sz[len - 1].style.transform = "scale(1.3)"
+            sz[len - 1].style.opacity = 1;
+            sz[len].style.left = "480px";
+
+            sync_szdiv()
+
+        }
+
+        function get_next() {
+            let give_up = sz[len];
+            sz.pop()
+            sz.unshift(give_up)
+            for (let i = 0; i < sz.length; i++) {
+                sz[i].style.zIndex = i;
+                sz[i].style.transform = "scale(1)"
+
+            }
+            sz[len - 2].style.left = "-80px";
+            sz[len - 1].style.zIndex = 10
+            sz[len - 1].style.left = "200px";
+            sz[len - 1].style.transform = "scale(1.3)"
+            sz[len - 1].style.opacity = 1;
+            sz[len].style.left = "480px";
+            sync_szdiv()
+
+
+        }
+
+
+        function sync_szdiv() {
+            for (let i = 0; i < szdiv.length; i++) {
+                if (szdiv[i].name == sz[len - 1].id)
+                    szdiv[i].style.background = "#e431fc"
+                else
+                    szdiv[i].style.background = "white"
+            }
+        }
+
+
 }
